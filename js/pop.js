12003555eeef094a0df27c9f8ca206396514a5f7
young-terps <70286054+young-terps@users.noreply.github.com>
2021-03-24 12:37:19 -0700

    Changes

---
+++ js/pop.js
@@ -0,0 +1,37 @@
+// MyPopUp
+// pop.js
+// Use .js to make a pop up
+// Author: Meg Mocek, Julia Bozzo, Phillip Guaracha, Luis Rivera
+// Created: Mar. 24, 2021
+// License: Public Domain
+
+//Pop up window that welcomes the user and provides a discription
+//Start timer when user opens page
+//After x amount of time a warning pops up
+//After x amount of time a pop up comes up that wont go away
+//User gets an option to dismiss the pop up incase of an emergency
+//Pop up stays for x amount of time, making user take a break
+//Pop up goes away after x amount of time
+//Timer resets
+
+
+
+//sortUserName
+function sortUserName() {
+  var userName = window.prompt("Hi, please tell me your name so I can fix it.");
+  console.log("userName =", userName);
+  //Split string to array
+  var nameArray = userName.split('');
+  console.log("nameArray =", nameArray);
+  //Sort array
+  var nameArraySort = nameArray.sort();
+  console.log("nameArraySort =", nameArraySort);
+  //Join array back to string
+  var nameSorted = nameArraySort.join('');
+  console.log("nameSorted =", nameSorted);
+  return nameSorted;
+}
+
+//OutPut
+document.writeln("Hey, I fixed your name: ",
+        sortUserName(), "</br>"); 
