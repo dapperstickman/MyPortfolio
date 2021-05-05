0f288a6cda8fac9db051da2b88e401cddb2bac96
young-terps <70286054+young-terps@users.noreply.github.com>
2021-05-05 09:59:04 -0700

    change

--- js/pop.js
+++ js/pop.js
@@ -13,25 +13,3 @@
 //Pop up stays for x amount of time, making user take a break
 //Pop up goes away after x amount of time
 //Timer resets
-
-
-
-// //sortUserName
-// function sortUserName() {
-//   var userName = window.prompt("Hi, please tell me your name so I can fix it.");
-//   console.log("userName =", userName);
-//   //Split string to array
-//   var nameArray = userName.split('');
-//   console.log("nameArray =", nameArray);
-//   //Sort array
-//   var nameArraySort = nameArray.sort();
-//   console.log("nameArraySort =", nameArraySort);
-//   //Join array back to string
-//   var nameSorted = nameArraySort.join('');
-//   console.log("nameSorted =", nameSorted);
-//   return nameSorted;
-// }
-//
-// //OutPut
-// document.writeln("Hey, I fixed your name: ",
-//         sortUserName(), "</br>"); 
