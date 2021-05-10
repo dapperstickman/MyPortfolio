010c4ca1b77a3344a9140d12a71562492f7cf2c4
young-terps <70286054+young-terps@users.noreply.github.com>
2021-05-10 10:06:51 -0700

    audio

--- js/pop.js
+++ js/pop.js
@@ -13,3 +13,51 @@
 //Pop up stays for x amount of time, making user take a break
 //Pop up goes away after x amount of time
 //Timer resets
+
+//Pop up 1
+var myVar;
+var x = new Boolean(false);
+  // if (x){
+      function alertFunc1() {
+        alarm();
+        // alert("You've been on for a while, would you like a break?");
+        if (confirm("You've been on for a while, would you like a break?")) { 
+          txt = "You pressed OK!"; 
+          // x = false;
+        } else { 
+          txt = "You pressed Cancel!"; 
+        var x = new Boolean(false);
+        }
+      }
+      function myFunction1() {
+        myVar = setTimeout(alertFunc1, 5000);
+      }
+    myFunction1();
+
+//Pop up 2
+var test;
+function myFunction2() {
+  test = setTimeout(alertFunc2, 10000);
+}
+function alertFunc2() {
+  alarm();
+  alert("Take a break");
+}
+myFunction2();
+
+//Pop up 3
+var test;
+function myFunction3() {
+  test = setTimeout(alertFunc3, 15000);
+}
+function alertFunc3() {
+  alarm();
+  alert("GET OFF, TAKE A BREAK!");
+}
+myFunction3();
+
+//audio
+function alarm() {
+  var audio = new Audio('./audio/alert.mp3');
+  audio.play();
+}
